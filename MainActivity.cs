95c7077ca0dbdbc2f44ca1dd951a1c357da93ee3
Ilnur Shafigullin <ilnur.shafigullin@tatar.ru>
2026-05-13 11:54:29 +0300

    invoke some native function from sqlite3_xamarin

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,11 +1,18 @@
+using System.Runtime.InteropServices;
+
 namespace AndroidApp
 {
     [Activity(Label = "@string/app_name", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        [DllImport("sqlite3_xamarin", EntryPoint = "sqlite3_initialize", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Initialize();
+
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
+            Initialize();
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
